0f918232f1b0e7bd193e3c117ef5a30676ddaea5
Frimpong Boateng <executivekfb27@gmail.com>
2023-01-23 18:42:24 +0000

    add javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+//element selectors
+
+let faqContainers = document.querySelectorAll(".faq-container");
+
+//Add Event Listener
+faqContainers.forEach(faqContainer => faqContainer.addEventListener("click", () => {
+    let faqAnswer = faqContainer.querySelector(".faq-answer");
+    let downArrow = faqContainer.querySelector(".down-arrow");
+
+    faqAnswer.classList.toggle("show-faq-answer");
+    downArrow.classList.toggle("up-arrow");
+    console.log("good");
+}));
